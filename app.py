25d161e5afacb1d559dccac97153a087eac2a5f5
Mohamed Essam <samm15846@gmail.com>
2024-05-30 00:42:05 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Load models
+diabetes_model = pickle.load(open('models/diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('models/heart_disease_model.sav', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diabetes', methods=['GET', 'POST'])
+def diabetes_prediction():
+    diab_diagnosis = ''
+    if request.method == 'POST':
+        Pregnancies = request.form['Pregnancies']
+        Glucose = request.form['Glucose']
+        BloodPressure = request.form['BloodPressure']
+        SkinThickness = request.form['SkinThickness']
+        Insulin = request.form['Insulin']
+        BMI = request.form['BMI']
+        DiabetesPedigreeFunction = request.form['DiabetesPedigreeFunction']
+        Age = request.form['Age']
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+        user_input = [float(x) for x in user_input]
+
+        diab_prediction = diabetes_model.predict([user_input])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+
+    return render_template('diabetes.html', result=diab_diagnosis)
+
+@app.route('/heart_disease', methods=['GET', 'POST'])
+def heart_disease_prediction():
+    heart_diagnosis = ''
+    if request.method == 'POST':
+        age = request.form['age']
+        sex = request.form['sex']
+        cp = request.form['cp']
+        trestbps = request.form['trestbps']
+        chol = request.form['chol']
+        fbs = request.form['fbs']
+        restecg = request.form['restecg']
+        thalach = request.form['thalach']
+        exang = request.form['exang']
+        oldpeak = request.form['oldpeak']
+        slope = request.form['slope']
+        ca = request.form['ca']
+        thal = request.form['thal']
+
+        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
+        user_input = [float(x) for x in user_input]
+
+        heart_prediction = heart_disease_model.predict([user_input])
+
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
+        else:
+            heart_diagnosis = 'The person does not have any heart disease'
+
+    return render_template('heart_disease.html', result=heart_diagnosis)
+
+if __name__ == "__main__":
+    app.run(debug=True)
